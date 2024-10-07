fa979089e58d737cb9ee6d57baaac2594265a7ae
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:46:59 +0700

    Add CreatePost in posts_controller.go🫸🌀✏️📗🐧🐳

---
+++ api/controllers/posts_controller.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"GoLang-JWT-Postgres-Mysql-Restful-API/api/auth"
+	"GoLang-JWT-Postgres-Mysql-Restful-API/api/models"
+	"GoLang-JWT-Postgres-Mysql-Restful-API/api/responses"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	post := models.Post{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	post.Prepare()
+	err = post.Validate()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	uid, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+	if uid != post.AuthorID {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+	postCreated, err := post.SavePost(server.DB)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
+	responses.JSON(w, http.StatusCreated, postCreated)
+}
