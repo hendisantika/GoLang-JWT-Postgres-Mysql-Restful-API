9eaa6fed1ce9f91163020c5f1340693ae383ebf1
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:45:43 +0700

    Add Home in home_controller.go🫸🌀✏️📗🐧🐳

---
+++ api/controllers/home_controller.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+	"GoLang-JWT-Postgres-Mysql-Restful-API/api/responses"
+	"net/http"
+)
+
+func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
+}
